d892d9de95ae0795bd1920c181b13af7bb9f308c
DevT02 <40608267+DevT02@users.noreply.github.com>
2020-05-10 01:25:02 -0400

    Rewrote this shitty program
    I suck at coding, use de4dot to clean nops and clean additional mutations, thanks to teix for explaining VERY useful stuff to me <3

--- IntptrPoint/PointRemover.cs
+++ IntptrPoint/PointRemover.cs
@@ -25,114 +25,52 @@ namespace IntptrPoint
                         IList<Instruction> instr = method.Body.Instructions;
                         for (int i = 0; i < instr.Count; i++)
                         {
-                            bool x = false;
-                            bool y = false;
+                            // CODE: new Point(1676, 3352).Y 
+                            // A POINT MUTATION IN DNSPY LOOKS LIKE:
+
+                            //19  0047    ldc.i4    0x68C 
+                            //20  004C    ldc.i4    0xD18
+                            //21  0051    newobj    instance void [System.Drawing]System.Drawing.Point::.ctor(int32, int32)
+                            //22  0056    stloc.s   V_69(69)
+                            //23  0058    ldloca.s  V_69(69)
+                            //24  005A    call    instance int32[System.Drawing]System.Drawing.Point::get_Y()
+
+
+                            // 2 instructions before Point::.ctor (constructor) is X value, 1 instruction before is Y value
+
+                            // 3 instructions ahead is .Y /.X  get_X obviously means .X   get_Y obviously means .Y
+
+                            // Here, we will try getting point via instruction 21 (the newobj opcode). If this exists, it is possible to check if there is point mutation
+
                             try
                             {
                                 if (instr[i + 3].OpCode != OpCodes.Call || instr[i].OpCode != OpCodes.Newobj || !instr[i].Operand.ToString().Contains("Point::.ctor") || !instr[i + 3].Operand.ToString().Contains("::get_") || !instr[i + 1].OpCode.Name.StartsWith("stloc") || !instr[i + 2].OpCode.Name.StartsWith("ldloca")) continue;
                             }
                             catch
                             {
-                                continue;
+                                continue; // If instruction is 0, 1, or 2, this will stop the OutOfRangeExeption. (skip)
                             }
 
-                            int index = i - 1;
-                            int index2 = i - 2;
-                            if (method.Body.Instructions[i + 3].Operand.ToString().Contains("::get_Y"))
+                            if (instr[i + 3].Operand.ToString().Contains("::get_X")) // IF .X
                             {
-                                y = true;
-                                while (method.Body.Instructions[index].OpCode.ToString().ToLower().Contains("nop"))
-                                {
-                                    index--;
-                                }
-                                while (method.Body.Instructions[index - 1].OpCode.ToString().ToLower().Contains("nop"))
-                                {
-                                    int intersepction = index - 1;
-                                    intersepction--;
-                                    index = intersepction;
-                                }
-                                if (method.Body.Instructions[index].OpCode.ToString().ToLower().Contains("ldc.i4"))
-                                {
-                                    Console.WriteLine("Found LDCi4, INDEX (X)");
-                                }
-                            }
-                            if (method.Body.Instructions[i + 3].Operand.ToString().Contains("::get_X"))
-                            {
-                                x = true;
-                                while (method.Body.Instructions[index2].OpCode.ToString().ToLower().Contains("nop"))
-                                {
-                                    index2--;
-                                }
-                                while (method.Body.Instructions[index2 - 1].OpCode.ToString().ToLower().Contains("nop"))
-                                {
-                                    int intersepction = index2 - 1;
-                                    intersepction--;
-                                    index2 = intersepction;
-                                }
-                                if (method.Body.Instructions[index2].OpCode.ToString().ToLower().Contains("ldc.i4"))
-                                {
-                                    Console.WriteLine("Found LDCi4, INDEX2 (Y)");
-                                }
-                            }
-
-                            int finalvalue = 0;
-                            if (x)
-                            {
-                                try
-                                {
-                                    finalvalue = method.Body.Instructions[index].GetLdcI4Value();
-                                }
-                                catch
-                                {
-                                    if (method.Body.Instructions[index].OpCode.ToString().ToLower().Contains("m1")) {
-                                        finalvalue = -1;
-                                    }
-                                    else if (!method.Body.Instructions[index].OpCode.ToString().Contains("ldc.i4"))
-                                    {
-                                        continue; //rip conv.i4
-                                    }
-                                    else if (method.Body.Instructions[index].OpCode.ToString().Replace("ldc.i4.i", "").Any(char.IsDigit))
-                                    {
-                                        Console.WriteLine(method.Body.Instructions[index].OpCode.ToString());
-                                        int held = Convert.ToInt32(method.Body.Instructions[index].OpCode.ToString().Replace("ldc.i4.i", ""));
-                                        finalvalue = held;
-                                    }
-                                }
-                                method.Body.Instructions[i].OpCode = OpCodes.Nop;
-                                method.Body.Instructions[i + 1].OpCode = OpCodes.Nop;
-                                method.Body.Instructions[i + 2].OpCode = OpCodes.Nop;
-                                method.Body.Instructions[i + 3].OpCode = OpCodes.Nop;
-                                method.Body.Instructions[i - 1].OpCode = OpCodes.Nop;
-                                method.Body.Instructions[index].Operand = finalvalue;
+                                if (!instr[i - 1].OpCode.ToString().Contains("ldc.i4"))
+                                    continue; //Opcode has to be an LDCi4 instruction
+                                instr[i].OpCode = OpCodes.Nop; //removes newobj
+                                instr[i + 1].OpCode = OpCodes.Nop; // removes stloc
+                                instr[i + 2].OpCode = OpCodes.Nop; // removes ldloca
+                                instr[i + 3].OpCode = OpCodes.Nop; // removes .X
+                                instr[i - 1].OpCode = OpCodes.Nop;
 
                             }
-                            if (y)
+                            if (instr[i + 3].Operand.ToString().Contains("::get_Y"))
                             {
-                                try
-                                {
-                                    finalvalue = method.Body.Instructions[index2].GetLdcI4Value();
-                                }
-                                catch
-                                {
-                                    if (method.Body.Instructions[index2].OpCode.ToString().ToLower().Contains("m1"))
-                                        finalvalue = -1;
-                                    else if (!method.Body.Instructions[index2].OpCode.ToString().Contains("ldc.i4"))
-                                    {
-                                        continue;
-                                    }
-                                    else if (method.Body.Instructions[index2].OpCode.ToString().Replace("ldc.i4.i", "").Any(char.IsDigit))
-                                    {
-                                        Console.WriteLine(method.Body.Instructions[index2].OpCode.ToString());
-                                        int held = Convert.ToInt32(method.Body.Instructions[index2].OpCode.ToString().Replace("ldc.i4.i", ""));
-                                        finalvalue = held;
-                                    }
-                                }
-                                method.Body.Instructions[i].OpCode = OpCodes.Nop;
-                                method.Body.Instructions[i + 1].OpCode = OpCodes.Nop;
-                                method.Body.Instructions[i + 2].OpCode = OpCodes.Nop;
-                                method.Body.Instructions[i + 3].OpCode = OpCodes.Nop;
-                                method.Body.Instructions[i - 2].OpCode = OpCodes.Nop;
-                                method.Body.Instructions[index2].Operand = finalvalue;
+                                if (!instr[i - 2].OpCode.ToString().Contains("ldc.i4"))
+                                    continue;
+                                instr[i].OpCode = OpCodes.Nop;
+                                instr[i + 1].OpCode = OpCodes.Nop;
+                                instr[i + 2].OpCode = OpCodes.Nop;
+                                instr[i + 3].OpCode = OpCodes.Nop;
+                                instr[i - 2].OpCode = OpCodes.Nop;
                             }
                             amount++;
                         }
